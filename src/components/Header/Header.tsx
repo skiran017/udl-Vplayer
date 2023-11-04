924504982ed17a2661d0d52e7ac7f428657551ef
skiran017 <skiran017@gmail.com>
2023-11-04 06:05:30 +0530

    feat: add Searchbar and Brand to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { MdOutlineSlowMotionVideo } from 'react-icons/md';
 import { ConnectButton } from 'arweave-wallet-kit';
 
 function Header() {
   return (
-    <Flex w="full" justifyContent="flex-end">
+    <Flex w="full" justifyContent="space-between" alignItems="center">
+      <Flex justifyContent="center" w="12%" alignItems="center">
+        <MdOutlineSlowMotionVideo
+          style={{ fontSize: '1.8rem', color: '#11999e' }}
+        />
+        <Text fontWeight={'bold'} fontSize="1.25rem" mx="6px">
+          VPlayer
+        </Text>
+      </Flex>
+      <Flex w="40%">
+        <InputGroup>
+          <Input
+            type="text"
+            placeholder="Search"
+            borderRadius="18px"
+            _focusVisible={{
+              borderColor: '#11999e',
+            }}
+          />
+          <InputRightElement pointerEvents="none">
+            <AiOutlineSearch style={{ fontSize: '1.4rem' }} />
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
       <ConnectButton accent="#11999e" />
     </Flex>
   );
