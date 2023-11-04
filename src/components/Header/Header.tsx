e9e05e645dbb77e2356d166599e4ab2b710eedc4
skiran017 <skiran017@gmail.com>
2023-11-05 05:03:30 +0530

    feat: add Renderer Menu component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
-import { Flex, Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
-import { AiOutlineSearch } from 'react-icons/ai'
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  Menu,
+  MenuItem,
+  MenuButton,
+  Button,
+  MenuList
+} from '@chakra-ui/react'
+import { AiOutlineSearch, AiOutlineDown } from 'react-icons/ai'
 import { MdOutlineSlowMotionVideo } from 'react-icons/md'
 import { ConnectButton } from 'arweave-wallet-kit'
 
 function Header() {
   // const id = 'IL5nfhl96Tvhxq0GpV7opSbX88T2l5eFJDaNudORbDs';
-  
+
   return (
     <Flex w="full" justifyContent="space-between" alignItems="center">
       <Flex justifyContent="center" w="12%" alignItems="center">
@@ -14,8 +25,8 @@ function Header() {
           VPlayer
         </Text>
       </Flex>
-      <Flex w="40%">
-        <InputGroup>
+      <Flex w="60%" justifyContent="space-evenly">
+        <InputGroup w="70%">
           <Input
             type="text"
             placeholder="Search"
@@ -28,6 +39,19 @@ function Header() {
             <AiOutlineSearch style={{ fontSize: '1.4rem' }} />
           </InputRightElement>
         </InputGroup>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<AiOutlineDown />} variant={'outline'} color="white " borderRadius="18px">
+            Choose Renderer
+          </MenuButton>
+          <MenuList fontWeight="semibold">
+            <MenuItem as="a" href="#">
+              VPlayer
+            </MenuItem>
+            <MenuItem as="a" href="#">
+              Others
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
       <ConnectButton accent="#11999e" />
     </Flex>
